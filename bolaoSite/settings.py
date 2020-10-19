750b8a4f77a67808a0aaa49ba331b8523b623434
Gabriel Reis <55334621+stackOvercrow@users.noreply.github.com>
2020-10-19 20:21:17 -0300

    Update settings.py

--- bolaoSite/settings.py
+++ bolaoSite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/home/stackovercrow/stackovercrow.pythonanywhere.com/bolaoApp/static/"
+STATIC_URL = "/bolaoApp/static/"
 STATIC_ROOT = "/home/stackovercrow/stackovercrow.pythonanywhere.com/bolaoApp/static/"
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
